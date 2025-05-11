27e5cfe0783413c542fc8d56c7f305a7f36b8fa2
Avinash Dola <2300030169@kluniversity.in>
2025-05-11 05:58:13 +0530

    Update ProjectApplication.java

--- src/main/java/com/fsd/sdp/project/ProjectApplication.java
+++ src/main/java/com/fsd/sdp/project/ProjectApplication.java
@@ -1,4 +1,8 @@
 package com.fsd.sdp.project;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
