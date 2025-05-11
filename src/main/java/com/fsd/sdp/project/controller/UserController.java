5156c2ee670ea03db4ab2781646388413aad8734
Avinash Dola <2300030169@kluniversity.in>
2025-05-11 06:04:53 +0530

    Update UserController.java

--- src/main/java/com/fsd/sdp/project/controller/UserController.java
+++ src/main/java/com/fsd/sdp/project/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@CrossOrigin()
+@CrossOrigin("https://reactfrontend-orcin.vercel.app/")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
