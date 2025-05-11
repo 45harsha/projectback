81ecd9f9b32ce38333911f339e4ccfb06d636270
Avinash Dola <2300030169@kluniversity.in>
2025-05-11 06:27:08 +0530

    Update WebConfig.java

--- src/main/java/com/fsd/sdp/project/WebConfig.java
+++ src/main/java/com/fsd/sdp/project/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Allow all paths starting with /api
         registry.addMapping("/api/**")
-                .allowedOrigins("https://reactfrontend-orcin.vercel.app", "http://localhost:3000")  // Allow both production and localhost for development
+                .allowedOrigins("https://reactfrontend-5rrvwa2vn-avinashs-projects-f6f8096c.vercel.app/", "http://localhost:3000")  // Allow both production and localhost for development
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow cookies and credentials
