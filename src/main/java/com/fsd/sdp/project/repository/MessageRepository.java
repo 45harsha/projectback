9c936237e567c859a94ae14ac527fd4639a71383
Avinash Dola <2300030169@kluniversity.in>
2025-05-13 11:22:44 +0530

    Update MessageRepository.java

--- src/main/java/com/fsd/sdp/project/repository/MessageRepository.java
+++ src/main/java/com/fsd/sdp/project/repository/MessageRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByGroupId(String groupId); // Changed from Long to String
+    List<Message> findByGroupId(String groupId); 
 }
