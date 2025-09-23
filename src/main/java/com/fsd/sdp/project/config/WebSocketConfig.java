64ef30271e241c07029f1cd45ce470dc5adf329f
45harsha <harsha11455j@gmail.com>
2025-09-23 18:56:25 +0530

    Update WebSocketConfig.java

--- src/main/java/com/fsd/sdp/project/config/WebSocketConfig.java
+++ src/main/java/com/fsd/sdp/project/config/WebSocketConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -18,7 +19,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:5174")
+                .setAllowedOriginPatterns(
+                    "http://localhost:8084",   // your frontend container
+                    "https://reactfrontend-orcin.vercel.app"  // production
+                )
                 .withSockJS();
     }
 }
