47b351415e21d56be7639a4f06f9b91e7b829ac5
45harsha <harsha11455j@gmail.com>
2025-09-23 19:19:53 +0530

    Update WebSocketConfig.java

--- src/main/java/com/fsd/sdp/project/config/WebSocketConfig.java
+++ src/main/java/com/fsd/sdp/project/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:8084") // Frontend dev server
+                .setAllowedOriginPatterns("*")  // Allow frontend connections
                 .withSockJS();
     }
 }
